9272730db78c5f87e6e8ccf9aaa2ee13b6787276
jmbradley <jmbradley808@gmail.com>
2018-11-11 20:19:00 -0500

    porting from local dev

---
+++ adlucha_x/blocked_domains.js
@@ -0,0 +1,6 @@
+var blocked_domains = [
+    "*://*.lb.usemaxserver.de/*",
+    "*://*.tracking.klickthru.com/*",
+    "*://*.zmedia.com/*",
+    "*://*.zv1.november-lax.com/*",
+    "*://*.espn.com/*"];
